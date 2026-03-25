c89ed07b5334d39f779b634e396911943d45e917
claytonzhuo <eafanzhuo@foxmail.com>
2026-03-25 21:35:27 +0800

    让greeting支持call命令

--- src/greeting.js
+++ src/greeting.js
@@ -5,21 +5,22 @@ const program = new Command();
 program
     .name('greeting')
     .description('Greeting to you')
-    .option('-a, --age <age>', 'Person age', 18)
-    .option('-p, --phone <phone>', 'Phone number', 110)
     .version('1.0.1')
 
 program
-    .command("start <name>")
+    .argument("<name>")
     .description("Greeting to your input name")
+    .option("-a, --age <age>", "Person age", '18')
+    .option("-p, --phone <phone>", "Phone number", '110')
     .action((name, options) => {
-        const { age } = options;
-        console.log(`Hello baby ${name}, age: ${age}`);
+        const { age, phone } = options;
+        console.log(`Hello baby ${name}, age: ${age}, phone: ${phone}`);
     });
 
 program
     .command('call <name>')
     .description('Call to your input name')
+    .option("-p, --phone <phone>", "Phone number", '110')
     .action((name, options) => {
         const { phone } = options;
         console.log(`Call ${name}, phone number: ${phone}`);
